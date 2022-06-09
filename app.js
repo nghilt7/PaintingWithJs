d1ca34940f024db5b9450e8aa88613bf9e74b3d1
nghilt7 <nghilt2307@gmail.com>
2022-06-09 09:54:33 +0700

    Done

---
+++ app.js
@@ -0,0 +1,100 @@
+const canvas = document.getElementById("jsCanvas");
+const ctx = canvas.getContext("2d");
+const color = document.querySelectorAll(".jsColor");
+const range = document.getElementById("jsRange");
+const mode = document.getElementById("jsMode");
+const saveBtn = document.getElementById("jsSave");
+
+canvas.width = 700;
+canvas.height = 700;
+
+ctx.fillStyle = "#fff";
+ctx.fillRect(0, 0, 700, 700);
+ctx.strokeStyle = "#2c2c2c";
+ctx.fillStyle = "#2c2c2c";
+ctx.lineWidth = 2.5;
+
+let painting = false;
+let filling = false;
+
+function stopPainting() {
+  painting = false;
+}
+
+function startPainting() {
+  painting = true;
+}
+
+function onMouseMove(e) {
+  const x = e.offsetX;
+  const y = e.offsetY;
+  if (!painting) {
+    ctx.beginPath();
+    ctx.moveTo(x, y);
+  } else {
+    ctx.lineTo(x, y);
+    ctx.stroke();
+  }
+}
+
+function handleColorClick(e) {
+  const color = e.target.style.backgroundColor;
+  ctx.strokeStyle = color;
+  ctx.fillStyle = color;
+}
+
+function handleRangeChange(e) {
+  const value = e.target.value;
+  ctx.lineWidth = value;
+}
+
+function handleModeClick() {
+  if (filling) {
+    filling = false;
+    mode.innerText = "Fill";
+  } else {
+    filling = true;
+    mode.innerText = "Paint";
+  }
+}
+
+function handleCanvasClick() {
+  if (filling) {
+    ctx.fillRect(0, 0, 700, 700);
+  }
+}
+
+function handleCM(e) {
+  e.preventDefault();
+}
+
+function handleSaveClick() {
+  const image = canvas.toDataURL();
+  const link = document.createElement("a");
+  link.href = image;
+  link.download = "PaintJS";
+  link.click();
+}
+
+if (canvas) {
+  canvas.addEventListener("mousemove", onMouseMove);
+  canvas.addEventListener("mousedown", startPainting);
+  canvas.addEventListener("mouseup", stopPainting);
+  canvas.addEventListener("mouseleave", stopPainting);
+  canvas.addEventListener("click", handleCanvasClick);
+  canvas.addEventListener("contextmenu", handleCM);
+}
+
+color.forEach((color) => color.addEventListener("click", handleColorClick));
+
+if (range) {
+  range.addEventListener("input", handleRangeChange);
+}
+
+if (mode) {
+  mode.addEventListener("click", handleModeClick);
+}
+
+if (saveBtn) {
+  saveBtn.addEventListener("click", handleSaveClick);
+}
